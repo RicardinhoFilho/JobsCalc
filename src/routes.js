cdcea0edeedd8e577046a166800f97fd54e96b9d
Ricardo Filho <ricardinhogiasson16@gmail.com>
2021-03-31 20:01:32 -0300

    Tornando Index dinâmico

--- src/routes.js
+++ src/routes.js
@@ -9,20 +9,94 @@ const views = __dirname + "/views/";
 const profile = {
 
     name: "Ricardo Filho",
-    avatar: "https://avatars.githubusercontent.com/u/74803287?s=400&u=d5d82e563f8b02b79447c6c832806a60dae9fb95&v=4",
-    'monthly-budget':"3000",
-    'days-per-week':"5",
-    'hours-per-day':"5",
-    'vacation-per-year':4
+    avatar: "https://github.com/RicardinhoFilho.png",
+    'monthly-budget': "3000",
+    'days-per-week': "5",
+    'hours-per-day': "5",
+    'vacation-per-year': 4,
+    "value-hour": 25
 
 }
 
-routes.get("/", (request, response) =>  response.render(views+"index.ejs"));
-routes.get("/job", (request, response) => response.render(views+"job.ejs"));
-routes.get("/job-edit", (request, response) => response.render(views+"job-edit.ejs"));
-routes.get("/profile", (request, response) =>  response.render(views+"profile.ejs", {profile: profile}));
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 2,
+        "total-hours": 1,
+        createdAt: Date.now()
+    },
+    {
+        id: 2,
+        name: "One-TwoProject",
+        "daily-hours": 3,
+        "total-hours": 47,
+        createdAt: Date.now()
+    }
 
-    
+];
+
+function remainingDays(job) {
+
+    const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed();//toFixed arredonda números(2.49 = 2, 2.50 = 3)
+
+    const createdDate = new Date(job.createdAt);//pega a data nos millisegundos e transforma em uma data formatada
+    //dueDay-> dia de vencimento
+    const dueDay = createdDate.getDate() + Number(remainingDays);//utilizo Number() porque toFixed transformou meu numero em string
+    //Data de vencimento
+    const dueDateInMs = createdDate.setDate(dueDay);//Data que começamos o projeto setDate(dia de vencimento)
+
+    const timeDiffInMs =  dueDateInMs - Date.now();
+    //transformar millisegundos em dias
+    const dayInMs = 1000 * 60 * 60 * 24;
+    const dayDiff = Math.floor((timeDiffInMs / dayInMs));
+
+    return dayDiff;
+
+}
+
+routes.get("/", (request, response) => {
+
+    //ajustes no job
+    //calculo d etempo restante
+
+    const uptedJobs = jobs.map((job) => {
+
+        const remaining = remainingDays(job);
+        const status = remaining <= 0 ? "done" : "progress";
+
+        return {
+            ...job,
+            remaining,
+            status,
+            budget: profile['value-hour'] * job['total-hours']
+        };
+    });
+
+
+
+    return response.render(views + "index.ejs", { jobs: uptedJobs })
+}
+);
+routes.get("/job", (request, response) => response.render(views + "job.ejs"));
+routes.post("/job", (request, response) => {
+    //console.log(request.body);//temos que habilitar -> server.use(express.urlencoded({ extend: true })) em server.js
+
+    const lastId = jobs[jobs.length - 1]?.id || 1;//pega o id do ultimo elemento, se não achar id será 1
+
+    const job = request.body;
+    jobs.push({
+        id: lastId + 1,
+        name: request.body.name,
+        "daily-hours": request.body["daily-hours"],
+        "total-hours": request.body["total-hours"],
+        createdAt: Date.now()
+    });
+    return response.redirect("/")
+})
+routes.get("/job-edit", (request, response) => response.render(views + "job-edit.ejs"));
+routes.get("/profile", (request, response) => response.render(views + "profile.ejs", { profile: profile }));
+
+
 module.exports = routes;
 
-   
--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const server = express();
-const routes =  require('./routes');
+const routes = require('./routes');
 
 //usando template engine
 server.set("views engine", "ejs");
 
 //habilitar arquivos estáticos _> pasta "public" 
 server.use(express.static("public"));
- 
- 
+
+//usar o request.body
+server.use(express.urlencoded({ extend: true }))
+
 //routes   
 server.use(routes);
-server.listen(3000, ()=>console.log("rodando")); 
+server.listen(3000, () => console.log("rodando")); 
